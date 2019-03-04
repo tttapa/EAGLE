d85150ab51ef860ec9d4f59ed364c3f66a74a7a7
Pieter <pieter.pas@outlook.com>
2019-03-04 23:57:27 +0100

    Test Siel's Tilt Correction

--- tilt-correction/test/test-TiltCorrection.cpp
+++ tilt-correction/test/test-TiltCorrection.cpp
@@ -1,5 +1,5 @@
-#include <Degrees.hpp>
 #include <AlmostEqual.hpp>
+#include <Degrees.hpp>
 #include <TiltCorrection.hpp>
 #include <gtest/gtest.h>
 
@@ -16,6 +16,25 @@ TEST(TiltCorrection, simple) {
         42 + 0.420455702855121,
         45 - 0.192352205961815,
         1.000000091161111,
+    }};
+    EXPECT_TRUE(isAlmostEqual(result, expected, 1e-15));
+}
+
+#include <tilt-correction.h>
+
+TEST(TiltCorrection, CCode) {
+    double z_             = 1.101718;
+    double theta          = 15_deg;
+    double phi            = 20_deg;
+    double psi            = 30_deg;
+    Quaternion q          = eul2quat({psi, theta, phi});
+    ColVector<2> position = {{42, 45}};
+    ColVector<2> result;
+    correct_tilt_pos(toCppArray(q), z_, position[0], position[1],
+                     &result[0][0], &result[1][0]);
+    ColVector<2> expected = {{
+        42 + 0.420455702855121,
+        45 - 0.192352205961815,
     }};
     EXPECT_TRUE(isAlmostEqual(result, expected, 1e-15));
 }
