c9ff244af4e9729a090e1efa8458c90c9919d42b
Pieter <pieter.pas@outlook.com>
2019-03-11 01:32:24 +0100

    Automatically build the C-Controllers if they're out of date @Bas update your `cmake` folder

--- cmake.example/FindCControllers.cmake
+++ cmake.example/FindCControllers.cmake
@@ -18,13 +18,22 @@ get_filename_component(CCONTROLLERS_LIBRARIES
     "${CCONTROLLERS_INCLUDE_DIR}/../x86_64/lib/libcontrollers.a" 
     ABSOLUTE
 )
+get_filename_component(CCONTROLLERS_PARAMS_AND_MATRICES
+    "${CCONTROLLERS_INCLUDE_DIR}/../Output/ParamsAndMatrices"
+    ABSOLUTE
+)
 message("CControllers include ${CCONTROLLERS_INCLUDE_DIR}")
 message("CControllers libraries ${CCONTROLLERS_LIBRARIES}")
+message("CControllers params ${CCONTROLLERS_PARAMS_AND_MATRICES}")
+
+add_custom_target(make-c-controllers make -C "${CCONTROLLERS_INCLUDE_DIR}/../")
 
 if(NOT TARGET CControllers)
     add_library(CControllers INTERFACE IMPORTED)
+    add_dependencies(CControllers make-c-controllers)
     set_target_properties(CControllers PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${CCONTROLLERS_INCLUDE_DIR}"
         INTERFACE_LINK_LIBRARIES "${CCONTROLLERS_LIBRARIES}"
+        INTERFACE_COMPILE_DEFINITIONS "PARAMS_AND_MATRICES_PATH=\"${CCONTROLLERS_PARAMS_AND_MATRICES}\""
     )
 endif()
