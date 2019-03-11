18584a1f88702451262c146be127e94a90902096
Pieter <pieter.pas@outlook.com>
2019-03-11 01:02:57 +0100

    Update altitude observer signature (no more tilt correction)

--- drone/src/C-code-wrappers/CKalmanObserver.cpp
+++ drone/src/C-code-wrappers/CKalmanObserver.cpp
@@ -1,7 +1,7 @@
 #include <C-code-wrappers/CKalmanObserver.hpp>
+#include <DroneStateControlOutput.hpp>
 #include <altitude-controller.h>
 #include <attitude-controller.h>
-#include <DroneStateControlOutput.hpp>
 
 namespace Attitude {
 CKalmanObserver::VecX_t CKalmanObserver::getStateChange(const VecX_t &x_hat,
@@ -19,10 +19,8 @@ CKalmanObserver::VecX_t CKalmanObserver::getStateChange(const VecX_t &x_hat,
                                                         const VecY_t &y_sensor,
                                                         const VecU_t &u) {
     VecX_t x_hat_cpy = x_hat;
-    Attitude::CKalmanObserver::VecX_t att = DroneAttitudeState{{1}}; // TODO
     updateAltitudeKFEstimate(toCppArray(x_hat_cpy), toCppArray(u),
-                             toCppArray(y_sensor), toCppArray(att),
-                             1);
+                             toCppArray(y_sensor), 1);
     return x_hat_cpy;
 }
 }  // namespace Altitude
